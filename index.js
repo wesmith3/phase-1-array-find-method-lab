ea97974774be8eb35548005841993fd2d188d38b
Wesley <wesmith314@gmail.com>
2023-09-18 06:22:32 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    const lastWin = record.find(obj => obj.result === "W")
+    if (lastWin) {
+        return lastWin.year;
+    } else {
+        return undefined;
+    }
+}
